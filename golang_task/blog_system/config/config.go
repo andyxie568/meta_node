6d63a3dc42fc13e5c920c6c798d5cf884ffd1922
andyxie568 <summer807@126.com>
2025-07-26 12:40:27 +0800

    update

--- golang_task/blog_system/config/config.go
+++
@@ -1,8 +0,0 @@
-package config
-
-import "time"
-
-const (
-	SignInDuration = 24 * time.Hour
-	JwtSecret      = "0a2a6bba0aa283c811d750e4c93ef589e736d5e9a570d53873c6b0a192072586549ad774708c59a9602faa810d772d7d083233d54c8e68bc493086ae612f5390"
-)
--- golang_task/blog_system/scripts/gorm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gentool -dsn "root:123456@tcp(127.0.0.1:3306)/blog" \
-  -tables "comments,posts,users" \
-  -outPath models/gen \
-  -fieldSignable
-
